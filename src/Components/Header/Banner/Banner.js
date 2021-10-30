cbe54511acecba2db8b394ea423d32926ac02301
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-10-30 14:33:40 +0600

    banner work done

--- src/Components/Header/Banner/Banner.js
+++ src/Components/Header/Banner/Banner.js
@@ -4,8 +4,16 @@ import './Banner.css'
 import { bannerbg } from '../../../img/banner1.jpg'
 const Banner = () => {
     return (
-        <div className="banner-container">
-            <h1>this is banner</h1>
+        <div className="banner-container d-lg-flex justify-content-start align-items-center flex-end ps-5">
+            {/* banner text */}
+            <div className="banner-text-container    text-start fw-bold text-uppercase ">
+                <h1>Welcome to <span className="text-danger"> Travel land   </span></h1>
+                <br></br>
+                <h1>Travel around the world<br></br> with us .</h1>
+                <br></br>
+                <h3>We are eagerly waiting for YOU</h3>
+
+            </div>
         </div>
     );
 };
