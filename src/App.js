d4ca82c57221364c38e7073ddc4dcf02a7a8ff54
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-10-30 20:24:45 +0600

    add service

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 //adding bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header';
+import AddService from './Components/Header/AddService/AddService';
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <AddService></AddService>
     </div>
   );
 }
---
+++ src/Components/Header/AddService/AddService.js
@@ -0,0 +1,40 @@
+//this component will keep the form to add more services
+//rook hook form will be used here 
+import './AddService.css';
+import React from 'react';
+import { useForm } from "react-hook-form";
+import { Placeholder } from 'react-bootstrap';
+const AddService = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    // function(onSubmit) to see the form data 
+    const onSubmit = data => {
+        reset();
+        console.log(data);
+    }
+    return (
+        <div className="add-service-container d-flex align-items-center justify-content-center flex-column ">
+
+            <div className="add-service-form-container   d-flex align-items-center justify-content-center  w-100">
+                <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column p-5 shadow">
+                    <h1 className="text-danger">Add a service</h1>
+                    {/* register your input into the hook by invoking the "register" function */}
+                    {/* --------------title field ------------------ */}
+                    <input defaultValue="" {...register("Title", { required: true },)} placeholder="Title" className="form-control" />
+
+                    {/* include validation with required or other standard HTML validation rules */}
+                    {/* ------------description field------------ */}
+                    <textarea defaultValue="" {...register("description", { required: true },)} placeholder="Description" className="form-control" />
+                    {/* ----------price ------- */}
+                    <input defaultValue="" {...register("price", { required: true },)} placeholder="Price" className="form-control" />
+                    {/* ------------image link */}
+                    <input defaultValue="" {...register("imgUrl", { required: true },)} placeholder="image-url" className="form-control" />
+                    {/* errors will return when field validation fails  */}
+                    {errors.exampleRequired && <span>This field is required</span>}
+                    <input type="submit" className="btn btn-outline-success" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default AddService;
