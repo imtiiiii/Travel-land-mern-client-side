36cfcabfc3e0b11c9636f2dc14b1a4645533250d
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-10-30 14:01:36 +0600

    added banner

---
+++ src/Components/Header/Banner/Banner.js
@@ -0,0 +1,13 @@
+//this is banner component
+import React from 'react';
+import './Banner.css'
+import { bannerbg } from '../../../img/banner1.jpg'
+const Banner = () => {
+    return (
+        <div className="banner-container">
+            <h1>this is banner</h1>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+//header component.
+//header will contain 2 more components
+//1-> bannner
+//2->navbar
+import React from 'react';
+import Banner from './Banner/Banner';
+
+const Header = () => {
+    return (
+        <div>
+
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Header;
