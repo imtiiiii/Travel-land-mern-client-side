10bbacd1c389fbb02b51a86f4cb5c4ed03aacc95
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-10-31 08:39:01 +0600

    blogs done

---
+++ src/Components/Home/Blogs/Blogs.js
@@ -0,0 +1,43 @@
+// this component will show all blogs one by one
+import './Blogs.css';
+import React from 'react';
+import { Card, CardGroup, Col } from 'react-bootstrap';
+import icon from '../../../img/writing1.png'
+const Blogs = (props) => {
+    const data = props.data;
+    console.log(data);
+    return (
+        <>
+            <Col className="gx-5 mb-5">
+                <CardGroup>
+                    <Card className="blogs-container shadow-sm">
+                        <div className="">
+                            <Card.Img variant="top" src={data.url} className="w-100 " />
+                        </div>
+                        {/* parent div for author icon and name  */}
+                        <div className="author-container d-lg-flex align-items-center justify-content-start w-100 ">
+                            {/* author name */}
+                            <div className="px-2">
+                                <h6 className="">{data.author}</h6>
+                            </div>
+
+                            {/* author  icon here  */}
+                            <div className=" d-lg-flex align-items-center justify-content-center">
+                                <img src={icon} className="author-icon" alt="icon"></img>
+                            </div>
+
+                        </div>
+
+                        <div className=" ps-lg-3 pt-lg-3">
+                            <Card.Title className="h1"><u>{data.title}</u></Card.Title>
+                        </div>
+
+
+                    </Card>
+                </CardGroup>
+            </Col>
+        </>
+    );
+};
+
+export default Blogs;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,8 +3,11 @@ import './Home.css';
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Service from './Service/Service';
+import Blogs from './Blogs/Blogs';
 
 const Home = () => {
+
+    // ------------ services--------------------
     // keep a state to store all the services
     const [services, setServices] = useState([]);
     //loading the data from db with useEffect
@@ -14,9 +17,19 @@ const Home = () => {
             //now set the data to the state
             .then(data => setServices(data));
     }, [])
+    // ------------- services end----------------
+
+    // ___________________Blogs____________________
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/blogs')
+            .then(res => res.json())
+            .then(data => setBlogs(data));
+    }, [])
     return (
         <div className="my-5">
             <h1 className="text-uppercase text-underline text-center text-danger"><u>Our services</u></h1>
+            {/* container for services */}
             <Container>
                 <Row lg={3} xs={1}>
                     {/* map through the services to show every single service */}
@@ -24,6 +37,19 @@ const Home = () => {
                         services.map(service => <Service key={service._id} data={service}></Service>)
                     }
                 </Row>
+            </Container>
+            {/* container for blogs */}
+            <Container >
+                <h1 className="text-center text-uppercase text-danger"><u>Fetured Blogs</u></h1>
+
+                <Row lg={3} xs={1}>
+                    {
+                        blogs.map(blog => <Blogs key={blog._id} data={blog} ></Blogs>)
+                    }
+
+                </Row>
+
+
             </Container>
         </div>
     );
