c766611354bf73aaf62054d98f5257ea7d93e2ed
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2021-10-31 13:22:35 +0600

    added firebase config

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,6 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+const firebaseInit = () => {
+    const initFirebase = initializeApp(firebaseConfig);
+}
